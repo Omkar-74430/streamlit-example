4b910d34a7899651c563c68c4f84e5ba998415c8
Omkar Khurd <93334241+Omkar-74430@users.noreply.github.com>
2024-02-20 12:40:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,81 @@
-import altair as alt
-import numpy as np
-import pandas as pd
+# libs
+from tensorflow.keras.applications import VGG16
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+# Set up Streamlit
 import streamlit as st
+from PIL import Image
+import numpy as np
+import tensorflow as tf
 
-"""
-# Welcome to Streamlit!
+# Load your trained model
+model_path = r"C:\project\fabric_authentication_model2.h5"
+model = tf.keras.models.load_model(model_path)
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+# Function to preprocess the uploaded image
+def preprocess_image(image_path):
+    img = Image.open(image_path)
+    img = img.resize((224, 224))
+    img = np.expand_dims(np.array(img) / 255.0, axis=0)
+    return img
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+# Function to make predictions
+def predict(image):
+    result = model.predict(image)
+    return result
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+# Streamlit app
+st.title("Fabric Authentication System using Deep Learning")
+st.sidebar.title("Options")
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+# Apply custom styles
+st.markdown(
+    """
+    <style>
+        body {
+            color: #333;
+            background: linear-gradient(to right, #ff6666, #ff8c66, #ffb366, #ffd966, #ffff66, #d9ff66, #b3ff66);
+            font-family: 'Arial', sans-serif;
+        }
+        .reportview-container {
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+            border-radius: 8px;
+            overflow: hidden;
+        }
+        .sidebar .sidebar-content {
+            background: #ffffff;
+        }
+        .sidebar .sidebar-content .block-container {
+            padding: 1rem;
+        }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
+# Upload image
+uploaded_file = st.file_uploader("Choose an image...", type="jpg")
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
+if uploaded_file is not None:
+    # Display the uploaded image
+    image = preprocess_image(uploaded_file)
+    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.success("Image successfully uploaded!")
 
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+    # Predict option
+    if st.button("Predict"):
+        # Make predictions
+        predictions = predict(image)
+        result_prob = predictions[0][0]
+
+        # Display results
+        if result_prob > 0.5:
+            st.success(f"Probability of the cloth being handmade is {result_prob * 100:.2f}%")
+            st.write('It is Handmade')
+        else:
+            st.error(f"Probability of the cloth being handmade is {result_prob * 100:.2f}%")
+            st.write('It is Machine-made cloth')
+
